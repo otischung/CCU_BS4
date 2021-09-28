31dad5ba1422e30f17a9dace8d113072c0e4fed8
otischung <my072814638@gmail.com>
2021-09-28 23:37:00 +0800

    CCU_BS4 init

---
+++ data_example.py
@@ -0,0 +1,40 @@
+login_data = {
+    "version": "0",
+    "id": "",
+    "password": "",
+    "term": "on",
+    "m": "0",
+}
+
+choose = {
+    "session_id": "",
+    "use_cge_new_cate": "1",
+    "page": "0",
+    "e": "0",
+    "grade": "2",
+    "grade2": "2",
+    "dept": "4104",
+    "use_cge_new_cate": "$use_cge_new_cate",
+}
+
+select = {
+    "session_id": "",
+    "dept": 4104,
+    "grade": 2,
+    "cge_cate": "",
+    "cge_subcate": "",
+    "page": 1,
+    "e": 0,
+    "m": 0,
+    "SelectTag": 1,
+    "4102001_01": 1,
+    "4102001_02": 1,
+    "4102003_01": 1,
+    "4102003_02": 1,
+    "4102013_01": 1,
+    "course": "4102062_01",
+    "4102062_01": 1,
+    "4102062_02": 1,
+}
+
+value = '[value="4102062_01"]'
